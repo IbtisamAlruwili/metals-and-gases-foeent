fcbcf3f41a19b49babdacfd27ecf9f95985cbff5
IbtisamAlruwili <Ibtisam.Ghazi.rae.js@tuwaiq.edu.sa>
2021-12-12 23:48:23 +0300

    forent1

---
+++ src/components/Product.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import "./Pruduct.css"
+import axios from "axios";
+
+export default function Courses({ token }) {
+  return (
+ <h1>Login</h1>  );
+}
